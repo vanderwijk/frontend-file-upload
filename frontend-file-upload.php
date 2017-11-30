80b837d1805ae5695c2b585d1dd694b821dd83e5
Johan van der Wijk <vanderwijk@users.noreply.github.com>
2017-11-30 17:36:26 +0100

    Support for filetype filtering

--- frontend-file-upload.php
+++ frontend-file-upload.php
@@ -28,6 +28,40 @@ function frontend_file_upload( $atts ) {
 
 	$attachment_filetype = esc_attr($a['filetype']);
 
+	if ( $attachment_filetype ) {
+		$allowed_file_types_array = explode(',', $attachment_filetype);
+		foreach ( $allowed_file_types_array as $allowed_file_type ) {
+			if ( $allowed_file_type == 'jpg' ) {
+				$allowed_file_types['jpg'] = 'image/jpg';
+			} else if ( $allowed_file_type == 'jpeg' ) {
+				$allowed_file_types['jpg'] = 'image/jpg';
+			} else if ( $allowed_file_type == 'gif' ) {
+				$allowed_file_types['jpg'] = 'image/gif';
+			} else if ( $allowed_file_type == 'png' ) {
+				$allowed_file_types['png'] = 'image/png';
+			} else if ( $allowed_file_type == 'psd' ) {
+				$allowed_file_types['psd'] = 'image/psd';
+			} else if ( $allowed_file_type == 'svg' ) {
+				$allowed_file_types['svg'] = 'image/svg+xml';
+			} else if ( $allowed_file_type == 'ai' ) {
+				$allowed_file_types['ai'] = 'application/illustrator';
+			} else if ( $allowed_file_type == 'pdf' ) {
+				$allowed_file_types['pdf'] = 'application/pdf';
+			}
+		}
+	} else {
+		$allowed_file_types = array( 
+			'jpg'  =>  'image/jpg',
+			'jpeg' =>  'image/jpeg',
+			'gif'  =>  'image/gif',
+			'png'  =>  'image/png',
+			'psd'  =>  'image/psd',
+			'svg'  =>  'image/svg+xml',
+			'ai'   =>  'application/illustrator',
+			'pdf'  =>  'application/pdf'
+		);
+	}
+
 	// Check that the nonce is valid, and the user can edit this post. Then upload the file.
 	if ( isset( $_POST['file_upload_nonce'], $_POST['post_id'] ) && wp_verify_nonce( $_POST['file_upload_nonce'], 'file_upload' ) && current_user_can( 'read' ) ) {
 
@@ -60,20 +94,6 @@ function frontend_file_upload( $atts ) {
 		require_once( ABSPATH . 'wp-admin/includes/media.php' );
 
 		// Let WordPress handle the upload.
-		if ( $attachment_filetype ) {
-			$allowed_file_types = array( $attachment_filetype );
-		} else {
-			$allowed_file_types = array( 
-				'jpg'  =>  'image/jpg',
-				'jpeg' =>  'image/jpeg',
-				'gif'  =>  'image/gif',
-				'png'  =>  'image/png',
-				'psd'  =>  'image/psd',
-				'svg'  =>  'image/svg+xml',
-				'ai'   =>  'application/illustrator',
-				'pdf'  =>  'application/pdf'
-			);
-		}
 		$overrides = array( 'test_form' => false, 'mimes' => $allowed_file_types );
 		// 'file_upload' is the name of the file input in the form below.
 		$attachment_id = media_handle_upload( 'file_upload', $_POST['post_id'], array( 'post_title' => $current_user->display_name, 'post_content' => $current_user->ID ), $overrides );
@@ -120,8 +140,6 @@ function frontend_file_upload( $atts ) {
 	});
 	</script>
 	<p>';
-
-	$content .= $allowed_file_types; 
 
 	if ( esc_attr($a['thumbnail']) === 'true' ) {
 		
